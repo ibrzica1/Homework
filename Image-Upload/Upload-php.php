8eb6261441dfbacdd3753aac47ccb4e440eabf66
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-09-17 18:14:41 +0100

    File upload and sql Homework

---
+++ Image-Upload/Upload-php.php
@@ -0,0 +1,39 @@
+<?php
+
+if(!isset($_FILES['profileImage']))
+{
+  die("Niste prosledili profilnu sliku");
+}
+
+$profileImage = $_FILES['profileImage'];
+
+require_once "models/Images.php";
+
+$image = new Images();
+
+$randomName = $image->generateRandomName('jpg');
+
+$imageFolder = "images/";
+
+if(!is_dir($imageFolder))
+{
+  mkdir($imageFolder, 0755, true);
+}
+
+if(!$image->isValidSize($profileImage))
+{
+  die("Slika je prevelika");
+
+}
+
+if(!$image->isValidDimension($profileImage))
+{
+  die("Image width cant be wider than 1920 or higher than 1024");
+}
+
+if(!$image->isValidExtension($profileImage))
+{
+  die("Format slike nije dobar");
+}
+
+$image->uploadImage($profileImage,$randomName);
